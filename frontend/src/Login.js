63226523525e317f532b710561223cff8b151a83
Surbhi <surbhikandhway12@gmail.com>
2026-04-26 19:00:31 +0530

    login fix

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,48 +1,67 @@
 import { useState } from "react";
-import axios from "axios";
+import API from "./api";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const navigate = useNavigate();
 
-  const [form, setForm] = useState({ email: "", password: "" });
+const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+const [form,setForm]=useState({
+ email:"",
+ password:""
+});
 
-    try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
-        form
-      );
+const handleLogin = async(e)=>{
+e.preventDefault();
 
-      localStorage.setItem("token", res.data.token);
-      localStorage.setItem("role", res.data.role);
+try{
 
-      navigate("/dashboard");
+const res = await API.post("/api/auth/login",form);
 
-    } catch {
-      alert("Login failed ❌");
-    }
-  };
+localStorage.setItem("token",res.data.token);
 
-  return (
-    <div className="h-screen flex items-center justify-center bg-indigo-600">
-      <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow w-80">
-        <h2 className="text-xl font-bold mb-3">Login</h2>
+// hardcode admin role because backend only sends token
+localStorage.setItem("role","admin");
 
-        <input placeholder="Email" className="p-2 border w-full mb-2"
-          onChange={(e)=>setForm({...form,email:e.target.value})}/>
+navigate("/dashboard");
 
-        <input type="password" placeholder="Password" className="p-2 border w-full mb-2"
-          onChange={(e)=>setForm({...form,password:e.target.value})}/>
+}
+catch(err){
+console.log(err.response?.data);
+alert("Login failed ❌");
+}
 
-        <button className="bg-indigo-600 text-white w-full py-2 rounded">
-          Login
-        </button>
-      </form>
-    </div>
-  );
+};
+
+return(
+<div className="h-screen flex items-center justify-center bg-indigo-600">
+<form onSubmit={handleLogin} className="bg-white p-6 rounded shadow w-80">
+
+<h2 className="text-xl font-bold mb-3">
+Login
+</h2>
+
+<input
+placeholder="Email"
+className="p-2 border w-full mb-2"
+onChange={(e)=>setForm({...form,email:e.target.value})}
+/>
+
+<input
+type="password"
+placeholder="Password"
+className="p-2 border w-full mb-2"
+onChange={(e)=>setForm({...form,password:e.target.value})}
+/>
+
+<button className="bg-indigo-600 text-white w-full py-2 rounded">
+Login
+</button>
+
+</form>
+</div>
+)
+
 }
 
 export default Login;
